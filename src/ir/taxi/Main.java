1fd40093f32fcaab6874f751dcb4df696c1c9ebd
Mahsa <mahsa.alikhany@gmail.com>
2021-10-02 17:52:25 +0330

    add case1 to main method.

--- src/ir/taxi/Main.java
+++ src/ir/taxi/Main.java
@@ -1,7 +1,26 @@
 package ir.taxi;
 
+import ir.taxi.enumeration.MainMenu;
+import ir.taxi.model.Taxi;
+
+import java.util.Scanner;
+
 /**
  * @author Mahsa Alikhani m-58
  */
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        MainMenu mainMenu = null;
+        mainMenu.showMainMenu();
+        int choice = scanner.nextInt();
+        switch (choice){
+            case 1:
+                System.out.println("Enter number of drivers:");
+                int numberOfDrivers = scanner.nextInt();
+                Taxi taxi = new Taxi();
+                taxi.addGroupOfDrivers(numberOfDrivers);
+                break;
+        }
+    }
 }
--- src/ir/taxi/model/Taxi.java
+++ src/ir/taxi/model/Taxi.java
@@ -4,4 +4,8 @@ package ir.taxi.model;
  * @author Mahsa Alikhani m-58
  */
 public class Taxi {
+
+    public void addGroupOfDrivers(int number){
+
+    }
 }
