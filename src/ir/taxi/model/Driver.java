4859a4606e5cf98e3e77356ac30aca48e6f39af4
Mahsa <mahsa.alikhany@gmail.com>
2021-10-02 19:48:19 +0330

    add properties to Driver class.

--- src/ir/taxi/model/Driver.java
+++ src/ir/taxi/model/Driver.java
@@ -1,7 +1,20 @@
 package ir.taxi.model;
 
+import java.sql.Date;
+
 /**
  * @author Mahsa Alikhani m-58
  */
 public class Driver {
+    private int id;
+    private String username;
+    private String name;
+    private String family;
+    private int phoneNumber;
+    private int nationalCode;
+    private Date birthDate;
+    private int carId;
+    private String plaque;
+    private int colorId;
+
 }
