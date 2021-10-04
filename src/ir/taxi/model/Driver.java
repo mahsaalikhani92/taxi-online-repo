4d7316c277f93b4e82a1704459465554087c8f86
Mahsa <mahsa.alikhany@gmail.com>
2021-10-04 11:13:14 +0330

    modify Person and Driver classes.

--- src/ir/taxi/model/Driver.java
+++ src/ir/taxi/model/Driver.java
@@ -5,73 +5,15 @@ import java.sql.Date;
 /**
  * @author Mahsa Alikhani m-58
  */
-public class Driver {
-    private String name;
-    private String family;
-    private String username;
-    private int phoneNumber;
-    private int nationalCode;
-    private Date birthDate;
+public class Driver extends Person{
+
     private int carId;
     private String plaque;
     private int colorId;
 
     public Driver(String name, String family, String username, int phoneNumber, int nationalCode, Date birthDate, String plaque) {
-        this.name = name;
-        this.family = family;
-        this.username = username;
-        this.phoneNumber = phoneNumber;
-        this.nationalCode = nationalCode;
-        this.birthDate = birthDate;
+        super(name, family, username, phoneNumber, nationalCode, birthDate);
         this.plaque = plaque;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getFamily() {
-        return family;
-    }
-
-    public void setFamily(String family) {
-        this.family = family;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public int getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(int phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public int getNationalCode() {
-        return nationalCode;
-    }
-
-    public void setNationalCode(int nationalCode) {
-        this.nationalCode = nationalCode;
-    }
-
-    public Date getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(Date birthDate) {
-        this.birthDate = birthDate;
     }
 
     public String getPlaque() {
--- src/ir/taxi/model/Person.java
+++ src/ir/taxi/model/Person.java
@@ -1,7 +1,72 @@
 package ir.taxi.model;
 
+import java.sql.Date;
+
 /**
  * @author Mahsa Alikhani m-58
  */
 public class Person {
+    private String name;
+    private String family;
+    private String username;
+    private int phoneNumber;
+    private int nationalCode;
+    private Date birthDate;
+
+    public Person(String name, String family, String username, int phoneNumber, int nationalCode, Date birthDate) {
+        this.name = name;
+        this.family = family;
+        this.username = username;
+        this.phoneNumber = phoneNumber;
+        this.nationalCode = nationalCode;
+        this.birthDate = birthDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getNationalCode() {
+        return nationalCode;
+    }
+
+    public void setNationalCode(int nationalCode) {
+        this.nationalCode = nationalCode;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
 }
