c39ba78e67674477d11aa24ed74e71d0a80ab3e2
Mahsa <mahsa.alikhany@gmail.com>
2021-10-04 11:14:41 +0330

    add constructor to Passenger class.

--- src/ir/taxi/model/Passenger.java
+++ src/ir/taxi/model/Passenger.java
@@ -1,7 +1,13 @@
 package ir.taxi.model;
 
+import java.sql.Date;
+
 /**
  * @author Mahsa Alikhani m-58
  */
-public class Passenger {
+public class Passenger extends Person {
+
+    public Passenger(String name, String family, String username, int phoneNumber, int nationalCode, Date birthDate) {
+        super(name, family, username, phoneNumber, nationalCode, birthDate);
+    }
 }
