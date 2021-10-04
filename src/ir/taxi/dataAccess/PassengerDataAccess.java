91e137475b33df8e288b2ad9f084ad8e147988c7
Mahsa <mahsa.alikhany@gmail.com>
2021-10-04 11:22:17 +0330

    add saveGroupOfPassengers method to PassengerDataAccess class.

--- src/ir/taxi/dataAccess/PassengerDataAccess.java
+++ src/ir/taxi/dataAccess/PassengerDataAccess.java
@@ -1,7 +1,34 @@
 package ir.taxi.dataAccess;
 
+import ir.taxi.model.Driver;
+import ir.taxi.model.Passenger;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
 /**
  * @author Mahsa Alikhani m-58
  */
-public class PassengerDataAccess {
+public class PassengerDataAccess extends DataBaseConnection{
+
+    public PassengerDataAccess() throws ClassNotFoundException, SQLException {
+        super();
+    }
+    public void saveGroupOfPassengers(List<Passenger> passengers) throws SQLException {
+        if(getConnection() != null){
+            String sqlQuery = "insert into passengers (username, name, family, phone_number, national_code, birth_date)" +
+                    "values(?, ?, ?, ?, ?, ?, ?)";
+            PreparedStatement stmt = getConnection().prepareStatement(sqlQuery);
+            for (Passenger item:passengers) {
+                stmt.setString(1, item.getUsername());
+                stmt.setString(2, item.getName());
+                stmt.setString(3, item.getFamily());
+                stmt.setInt(4, item.getPhoneNumber());
+                stmt.setInt(5, item.getNationalCode());
+                stmt.setDate(6, item.getBirthDate());
+            }
+        }
+        return;
+    }
 }
