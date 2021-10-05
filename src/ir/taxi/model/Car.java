3a7f870f00628181d56243ff8763f0f4f2f7dc09
Mahsa <mahsa.alikhany@gmail.com>
2021-10-05 16:36:24 +0330

    create Car class.

--- src/ir/taxi/model/Car.java
+++ src/ir/taxi/model/Car.java
@@ -1,10 +1,41 @@
 package ir.taxi.model;
 
+import ir.taxi.enumeration.CarColor;
+
 /**
  * @author Mahsa Alikhani m-58
  */
 public class Car {
     private int carId;
     private String model;
+    private CarColor carColor;
 
+    public Car(String model, CarColor carColor) {
+        this.model = model;
+        this.carColor = carColor;
+    }
+
+    public int getCarId() {
+        return carId;
+    }
+
+    public void setCarId(int carId) {
+        this.carId = carId;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public CarColor getCarColor() {
+        return carColor;
+    }
+
+    public void setCarColor(CarColor carColor) {
+        this.carColor = carColor;
+    }
 }
