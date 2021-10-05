ef3d239b9816b712291d50267b5dcde421f63964
Mahsa <mahsa.alikhany@gmail.com>
2021-10-05 14:42:53 +0330

    modify constructor in Passenger class.

--- src/ir/taxi/model/Passenger.java
+++ src/ir/taxi/model/Passenger.java
@@ -12,9 +12,23 @@ public class Passenger extends Person {
     private int balance;
     private Status status;
 
-    public Passenger(String name, String family, String username, int phoneNumber, int nationalCode, Date birthDate, int balance, Status status) {
+    public Passenger(String name, String family, String username, int phoneNumber, int nationalCode, Date birthDate) {
         super(name, family, username, phoneNumber, nationalCode, birthDate);
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
         this.balance = balance;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
         this.status = status;
     }
 }
