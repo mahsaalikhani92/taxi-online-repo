d22f4705702739698c03784ade201743633d792d
Mahsa <mahsa.alikhany@gmail.com>
2021-10-07 16:39:35 +0330

    add showVehicleMenu method to Vehicle class.

--- src/ir/taxi/enumeration/Vehicle.java
+++ src/ir/taxi/enumeration/Vehicle.java
@@ -4,4 +4,24 @@ package ir.taxi.enumeration;
  * @author Mahsa Alikhani m-58
  */
 public enum Vehicle {
+    CAR("1. Take a car"),
+    MOTORCYCLE("2. Take a motor cycle"),
+    VAN("3. Take a van"),
+    RV("4. Take a RV");
+
+    private String expression;
+
+    Vehicle(String expression) {
+        this.expression = expression;
+    }
+
+    public String getExpression() {
+        return expression;
+    }
+
+    public void showVehicleMenu(){
+        for (Vehicle item:Vehicle.values()) {
+            System.out.println(item.getExpression());
+        }
+    }
 }
