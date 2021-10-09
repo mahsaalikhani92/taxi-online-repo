1305f05f24dc6f08e26bb190e7470922aa5ffe52
Mahsa <mahsa.alikhany@gmail.com>
2021-10-09 21:54:20 +0330

    created TripDataAccess

---
+++ src/ir/taxi/dataAccess/TripDataAccess.java
@@ -0,0 +1,13 @@
+package ir.taxi.dataAccess;
+
+import java.sql.SQLException;
+
+/**
+ * @author Mahsa Alikhani m-58
+ */
+public class TripDataAccess extends DataBaseAccess{
+
+    public TripDataAccess() throws ClassNotFoundException, SQLException {
+        super();
+    }
+}
