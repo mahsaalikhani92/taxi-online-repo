322cc6fd25c608802152547c4f4503ecae4e224c
Mahsa <mahsa.alikhany@gmail.com>
2021-10-09 13:57:10 +0330

    added CONFIRM, FINISHED, EXIT to DriverLogin Menu

--- src/ir/taxi/enumeration/DriverLoginMenu.java
+++ src/ir/taxi/enumeration/DriverLoginMenu.java
@@ -4,4 +4,17 @@ package ir.taxi.enumeration;
  * @author Mahsa Alikhani m-58
  */
 public enum DriverLoginMenu {
+    CONFIRM("1. Confirm cash receipt"),
+    FINISHED("2. Travel finished"),
+    EXIT("3 .Exit");
+
+    private String expression;
+
+    DriverLoginMenu(String expression) {
+        this.expression = expression;
+    }
+
+    public String getExpression() {
+        return expression;
+    }
 }
