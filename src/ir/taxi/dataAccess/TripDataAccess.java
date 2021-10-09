abcd25c0f1019a7d9fcf5e5068eaa4fd59e68cef
Mahsa <mahsa.alikhany@gmail.com>
2021-10-09 22:27:36 +0330

    add findPayStatusByDriverUsername to TripDataAccess

--- src/ir/taxi/dataAccess/TripDataAccess.java
+++ src/ir/taxi/dataAccess/TripDataAccess.java
@@ -1,6 +1,11 @@
 package ir.taxi.dataAccess;
 
+import ir.taxi.enumeration.PayStatus;
+import ir.taxi.enumeration.TripStatus;
+
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * @author Mahsa Alikhani m-58
@@ -9,5 +14,18 @@ public class TripDataAccess extends DataBaseAccess{
 
     public TripDataAccess() throws ClassNotFoundException, SQLException {
         super();
+    }
+
+    public PayStatus findPayStatusByDriverUsername(String username) throws SQLException {
+        PayStatus payStatus = null;
+        if(getConnection() != null){
+            Statement statement = getConnection().createStatement();
+            ResultSet resultSet = statement.executeQuery(String.format("select pay_status from trip inner join drivers" +
+                    "on trip.driver_fk = drivers.driver_id where drivers.username = %s", username));
+            while (resultSet.next()){
+                payStatus = PayStatus.valueOf(resultSet.getString("pay_status"));
+            }
+        }
+        return payStatus;
     }
 }
