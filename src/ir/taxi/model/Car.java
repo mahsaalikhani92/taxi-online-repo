1216b7f69193bdadeb38d0ddd33fe9c3141839ed
Mahsa <mahsa.alikhany@gmail.com>
2021-10-10 12:22:52 +0330

    added equals and hashCode to Car

--- src/ir/taxi/model/Car.java
+++ src/ir/taxi/model/Car.java
@@ -1,5 +1,7 @@
 package ir.taxi.model;
 
+import java.util.Objects;
+
 /**
  * @author Mahsa Alikhani m-58
  */
@@ -35,5 +37,18 @@ public class Car {
 
     public void setCarColor(String carColor) {
         this.carColor = carColor;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return id == car.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
